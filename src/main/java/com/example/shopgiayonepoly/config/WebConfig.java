55877c899323003fe7736b86019e005ce7d9b2b8
BuiVanThiet <thietbuivan2512@gmail.com>
2024-12-13 06:01:17 +0700

    Thiet push, ngay13thang12, 06:02

--- src/main/java/com/example/shopgiayonepoly/config/WebConfig.java
+++ src/main/java/com/example/shopgiayonepoly/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.shopgiayonepoly.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
